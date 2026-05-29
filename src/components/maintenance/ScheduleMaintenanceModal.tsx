dfac2c5f7f553b94ea1ca224ed6ced513b05a30e
Ferrey Adinarta <ferreyadinarta5@gmail.com>
2026-05-29 13:20:08 +0700

    fix: stack datetime inputs vertically so they don't truncate in modal

--- src/components/maintenance/ScheduleMaintenanceModal.tsx
+++ src/components/maintenance/ScheduleMaintenanceModal.tsx
@@ -166,7 +166,7 @@ export default function ScheduleMaintenanceModal({
 
           {/* Window */}
           <div className="flex flex-col gap-2">
-            <div className="grid grid-cols-2 gap-3">
+            <div className="flex flex-col gap-4">
               <div className="flex flex-col gap-2">
                 <label className="text-xs font-semibold uppercase tracking-[0.08em]" style={{ color: "#3d3830" }}>
                   Starts
